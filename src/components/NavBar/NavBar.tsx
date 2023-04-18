72b484910dc9e99eef2f47fb7e3c11638c6de3db
Ahmed Maher <ahmedelseaidy22@gmail.com>
2023-04-18 11:27:31 +0200

    Editing Routing options.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import WebsiteLogo from "../../assets/images/LSPWebLogo.png";
-import { Link } from "react-router-dom";
+import { NavLink, NavLinkProps, Link } from "react-router-dom";
 import { categories } from "../../core/services";
 import { BiCategory, BiCartAlt, BiSearchAlt, BiHomeAlt } from "react-icons/bi";
 import { TiInfoLarge } from "react-icons/ti";
@@ -10,18 +10,21 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <section className="container-fluid">
-        <Link className="navbar-brand" to="/pages/home">
+        <NavLink className="navbar-brand" to="/pages/home">
           <img src={WebsiteLogo} alt="Website_Logo" />
-        </Link>
+        </NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <section className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/pages/home">
-                <BiHomeAlt /> Home
-              </Link>
+              {
+                // @ts-ignore
+                <NavLink className="nav-link" to="/pages/home" activeClassName="active">
+                  <BiHomeAlt /> Home
+                </NavLink>
+              }
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -65,14 +68,20 @@ const NavBar = () => {
               </ul>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/pages/cart">
-                <BiCartAlt /> Cart
-              </Link>
+              {
+                // @ts-ignore
+                <NavLink className="nav-link" to="/pages/cart" activeClassName="active">
+                  <BiCartAlt /> Cart
+                </NavLink>
+              }
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/pages/about">
-                <TiInfoLarge /> AboutUs
-              </Link>
+              {
+                // @ts-ignore
+                <NavLink className="nav-link" to="/pages/about" activeClassName="active">
+                  <TiInfoLarge /> AboutUs
+                </NavLink>
+              }
             </li>
           </ul>
         </section>
--- src/pages/OptionsPage/OptionsPage.tsx
+++ src/pages/OptionsPage/OptionsPage.tsx
@@ -1,18 +1,24 @@
 import { Link, Outlet } from "react-router-dom";
-import './OptionsPage.css'
+import "./OptionsPage.css";
 
 const OptionsPage = () => {
   return (
     <section className="options-page">
       <aside className="options-aside-bar">
         <ul>
-          <Link to="/pages/options/create-book"><li>Add Book</li></Link>
-          <Link to="/pages/options/user-accounts"><li>Users Account</li></Link>
-          <Link to="/pages/options/borrow-requests"><li>Borrow Request</li></Link>
+          <Link to="/pages/options/create-book">
+            <li>Add Book</li>
+          </Link>
+          <Link to="/pages/options/user-accounts">
+            <li>Users Account</li>
+          </Link>
+          <Link to="/pages/options/borrow-requests">
+            <li>Borrow Request</li>
+          </Link>
         </ul>
       </aside>
       <section className="options-page-body">
-        <Outlet/>
+        <Outlet />
       </section>
     </section>
   );
